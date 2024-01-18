b0150c3902066d5237574635fb66b02dccb7f9bb
HarmonyMorse <harmonythedev@gmail.com>
2024-01-18 09:11:12 -0800

    Add home path to urlpatterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+  path('', views.home, name='home'),
   path('sandbox/', views.sandbox, name='sandbox'),
   
   # Auth paths
