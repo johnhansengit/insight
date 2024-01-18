ab6fb8b84701ba36f4834e1040bacde602e7ed74
HarmonyMorse <harmonythedev@gmail.com>
2024-01-17 20:37:29 -0800

    Refactor signup view to use custom form and update user profile

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from .models import Profile
+
 
 # Create your views here.
 def sandbox(request):
-    return render(request, 'sandbox.html')
+    return render(request, "sandbox.html")
+
 
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get("email")
+            user.profile.first_name = form.cleaned_data.get("first_name")
+            user.profile.last_name = form.cleaned_data.get("last_name")
+            user.save()
             login(request, user)
-            # TODO: redirect to desired page
-            return redirect('sandbox')
-        else:
-            error_message = 'Invalid sign up - try again'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+            return redirect("sandbox")
+    form = SignUpForm()
+    context = {"form": form}
+    return render(request, "registration/signup.html", context)
